1055c947d7868a24e45ce1335d80c05a22dac7c9
Andriy <andriy.shakh@epitech.eu>
2020-04-29 18:05:56 +0200

    Stockage réponse quiz

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -7,20 +7,20 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Question;
 use App\Entity\Categorie;
 use App\Entity\Reponse;
+use App\Entity\Game;
 use App\Form\QuestionType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * @Route("/game", name="game")
 */
 class GameController extends AbstractController
 {
-    public function _construct() {
-
-    }
-
     /**
      * @Route("/{cat_id}", name="_cat")
      */
@@ -55,7 +55,6 @@ class GameController extends AbstractController
      */
     public function question($cat_id, $quest_id, Request $request)
     {
-
         // Retrieve categorie
         $repository = $this->getDoctrine()->getRepository(Categorie::class);
         $categorie = $repository->findOneBy([
@@ -69,15 +68,85 @@ class GameController extends AbstractController
             'idCategorie' => $cat_id,
         ]);
 
-
         // Retrieve answers
         $repository = $this->getDoctrine()->getRepository(Reponse::class);
         $answers = $repository->findBy([
             'idQuestion' => $quest_id,
         ]);
-        // dd($answers);
+
+        $form = $this->createFormBuilder()
+            ->add('answer', ChoiceType::class, array(
+                'choices'  => array(
+                    $answers[0]->getReponse() => $answers[0]->getId(),
+                    $answers[1]->getReponse() => $answers[1]->getId(),
+                    $answers[2]->getReponse() => $answers[2]->getId(),
+                ),
+                'expanded' => true,
+                'label' => false
+            ))
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success float-right'],
+                'label' => "Validate"
+                ])
+            ->getForm();
+
+        $answer = new Game();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get all data
+            $data = $form->getData();
+            $temp_id = $request->cookies->get('temp_id');
+
+            // dd($this->getUser()->getId());
+            if ($this->getUser()) {
+                $answer->setUserId($this->getUser()->getId());
+            }
+            else {
+                if ($temp_id) {
+                    $answer->setTempId($temp_id);
+                }
+                else {
+                    // Set cookie if doesn't exists
+                    $response = new Response();
+    
+                    $cookieValue1 = rand(0, 2147483647);
+                    $cookieValue2 = rand(0, 2147483647);
+                    $cookieValue = $cookieValue1 . $cookieValue2;
+                    
+                    $cookie = new Cookie("temp_id", $cookieValue);
+                    $response->headers->setCookie($cookie);
+                    $response->send();
+                }
+            }
+            $answer->setQuestionId($quest_id);
+
+            $answer_given = $repository->findOneBy([
+                'id' => $data['answer'],
+            ]);
+            $answer->setAnswer($answer_given->getReponseExpected());
+
             
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($answer);
+            $entityManager->flush();
+
+            // if ($answer_given->getReponseExpected() == true){
+            //     $this->addFlash('notice', 'Bien ouèj !');
+            // }
+            // else {
+            //     $this->addFlash('notice', 'Aïe...');
+            // }
+    
+            return $this->redirectToRoute('home');
+        }
+
+
         return $this->render('game/question.html.twig', [
+            'form' => $form->createView(),
             'categorie' => $categorie,
             'question' => $question,
             'answers' => $answers
---
+++ src/Entity/Game.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $temp_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $question_id;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $answer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getTempId(): ?int
+    {
+        return $this->temp_id;
+    }
+
+    public function setTempId(?int $temp_id): self
+    {
+        $this->temp_id = $temp_id;
+
+        return $this;
+    }
+
+    public function getQuestionId(): ?int
+    {
+        return $this->question_id;
+    }
+
+    public function setQuestionId(int $question_id): self
+    {
+        $this->question_id = $question_id;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?bool
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(bool $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+}
